e3ba1eee8e5b6a5a6d0576de0fb16da81ef02012
Lee Hoyong <igotoweb@gmail.com>
2020-02-12 14:38:29 +0900

    add travis ci setting

--- test/index.js
+++ test/index.js
@@ -1,11 +1,13 @@
+var reporter = require("jasmine-json-reporter");
+jasmine.getEnv().addReporter(reporter);
 global.FILL_ME_IN = "Fill this value in";
 
-describe('Bare Minimum Requirements', function() {
-  require('../koans/AboutExpects.js');
-  require('../koans/AboutArrays.js');
-  require('../koans/AboutFunctions.js');
-  require('../koans/AboutObjects.js');
-  require('../koans/AboutArrayMethods.js')
+describe("Bare Minimum Requirements", function() {
+  require("../koans/AboutExpects.js");
+  require("../koans/AboutArrays.js");
+  require("../koans/AboutFunctions.js");
+  require("../koans/AboutObjects.js");
+  require("../koans/AboutArrayMethods.js");
 });
 
 /*
